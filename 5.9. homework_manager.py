df9ef95e459c427d099f6c75a49d4f3eef5b9008
daniltarasov <59774430+daniltarasov@users.noreply.github.com>
2020-03-04 02:55:56 +0300

    Add files via upload

--- 5.9. homework_manager.py
+++ 5.9. homework_manager.py
@@ -1,9 +1,14 @@
 import time
 
+NUM_RUNS = 10  # число прогонов
+
 
 class TimeThis:
     def __init__(self, num_runs):  # конструктору передаем число прогонов
         self.num_runs = num_runs
+        self.avg_time = 0
+        self.t0 = 0
+        self.t1 = 0
 
     def __call__(self, func):  # оборачиваемую функцию передаем методу __call__
         def wrapper(*args, **kwargs):  # а аргументы - внутренней функции
@@ -14,15 +19,46 @@ class TimeThis:
                 t1 = time.time()
                 avg_time += (t1 - t0)
             avg_time /= self.num_runs
-            print("Выполнение заняло %.5f секунд" % avg_time)
+            print("[Декоратор] Выполнение заняло %.5f секунд" % avg_time)
 
         return wrapper
 
+    def __enter__(self):
+        """
+        Реализация контекстного менеджера.
+        на входе фиксируем время начала выполнения блока
+        """
+        self.t0 = time.time()
 
-@TimeThis(10)
-def f(iter):
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        """
+        на выходе фиксируем время окончания выполнения блока и рассчитываем среднее значение
+        """
+        self.t1 = time.time()
+        self.avg_time += (self.t1 - self.t0) / self.num_runs
+        print("[Контекстный менеджер] Выполнение заняло %.5f секунд" % self.avg_time)
+
+
+def foo(iter):
+    for j in range(iter):
+        pass
+
+@TimeThis(NUM_RUNS)
+def bar(iter):
     for j in range(iter):
         pass
 
 
-f(1_000_000)
+
+def main():
+    with TimeThis(NUM_RUNS):
+        for _ in range(NUM_RUNS):
+            foo(1000000)
+
+    bar(1000000)
+
+
+
+if __name__ == "__main__":
+    main()
